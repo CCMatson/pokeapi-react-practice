627feeb2c03e23acfe1e571e5464d9c72777373a
CCMatson <catherine.matson@gmail.com>
2023-03-13 23:32:28 -0700

    update code on details card

--- src/components/PokeDetails/PokeDetails.jsx
+++ src/components/PokeDetails/PokeDetails.jsx
@@ -16,52 +16,58 @@ const PokeDetails = () => {
     fetchDetails()
   }, [location.state.poke.url])
 
-  if (!pokePage) return <p>Loading...</p>
+  if (!pokePage) return
 
-  const { sprites, name, height, weight, base_experience,species , stats ,moves } = pokePage;
+  const { sprites, name, height, weight, base_experience, species, stats, moves } = pokePage;
+
 
   return (
     <>
-      <h1 className="welcome">Get to know {name}</h1>
-      <section>
-        <div className="pokeCard">
-          <div className="background">
-            <img src={sprites && sprites.front_default} alt="" />
-            <img src={sprites && sprites.back_default} alt="" />
-          </div>
-          <div className="content">
-            <p className="name">{name}</p>
-            <p>Height: {height}</p>
-            <p>Weight: {weight}</p>
-            <p>Base experience: {base_experience}</p>
-            {species && <p>Species: {species.name}</p>}
-          </div>
-          <>
-          </>
-          <Link to='/list' className="return">Back to the Pokedex</Link>
-          
-          { stats && (
-          <div className="stats">
-            <p>{name}'s stats:</p>
-            {stats.map(stat =>
-              <li>{stat.stat.name} @ {stat.base_stat} </li>)}
-          </div>
-          )}
-        </div>
-        {moves && (
-        <div className="bonus-content">
-          <p className="name">{name}'s moves</p>
-          <div className="moves">
-            {moves.map(move =>
-              <li>{move.move.name} / </li>
+      {/* {pokePage.length ? */}
+        <>
+          <h1 className="welcome">Get to know {name}</h1>
+          <section>
+            <div className="pokeCard">
+              <div className="background">
+                <img src={sprites && sprites.front_default} alt="" />
+                <img src={sprites && sprites.back_default} alt="" />
+              </div>
+              <div className="content">
+                <p className="name">{name}</p>
+                <p>Height: {height}</p>
+                <p>Weight: {weight}</p>
+                <p>Base experience: {base_experience}</p>
+                {species && <p>Species: {species.name}</p>}
+              </div>
+              <>
+              </>
+              <Link to='/list' className="return">Back to the Pokedex</Link>
+
+              {stats && (
+                <div className="stats">
+                  <p>{name}'s stats:</p>
+                  {stats.map(stat =>
+                    <li>{stat.stat.name} @ {stat.base_stat} </li>)}
+                </div>
+              )}
+            </div>
+            {moves && (
+              <div className="bonus-content">
+                <p className="name">{name}'s moves</p>
+                <div className="moves">
+                  {moves.map(move =>
+                    <li>{move.move.name} / </li>
+                  )}
+                </div>
+              </div>
             )}
-          </div>
-
-        </div>
-
-
-        )}
-      </section>
+          </section>
+        </>
+        {/* : */}
+        <>
+          {/* <h1>Loading</h1> */}
+        </>
+      
     </>
   )
 }
